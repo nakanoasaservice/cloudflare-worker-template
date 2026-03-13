a11f3b5a60e5a3d3fd9e5bf401de4ffe2a652c81
Nakano as a Service <nakanoasaservice@gmail.com>
2026-03-13 20:06:24 +0900

    feat: upgrade vitest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,4 @@
-import {
-	createExecutionContext,
-	env,
-	SELF,
-	waitOnExecutionContext,
-} from "cloudflare:test";
+import { env, exports } from "cloudflare:workers";
 import { describe, expect, it } from "vitest";
 
 import worker from "../src/index";
@@ -16,15 +11,12 @@ describe("Hello World worker", () => {
 	it("responds with Hello World! (unit style)", async () => {
 		const request = new IncomingRequest("http://example.com");
 		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
+		const response = await worker.fetch(request, env);
 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 
 	it("responds with Hello World! (integration style)", async () => {
-		const response = await SELF.fetch("https://example.com");
+		const response = await exports.default.fetch("https://example.com");
 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 });
